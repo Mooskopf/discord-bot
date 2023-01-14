d29d48b445bb07df0456dafb0723ee78ab93a61c
Mooskopf <florinbrueske@web.de>
2023-01-14 14:51:19 +0100

    roll update

--- roll.py
+++ roll.py
@@ -14,14 +14,14 @@ def roll_numbers(data):
     if not arr[1].isdigit() or not arr[2].isdigit():     #Checks if the inputs are numbers
       return("Bitte nur ganze Zahlen angeben")
     else:
-      n1 = int(arr[1])-1
+      n1 = int(arr[1])
       n2 = int(arr[2])+1
       n = random.randrange(n1, n2)
       return(f'{str(data.author)[:-5]} hat eine {n} gewürfelt')
  
 #Rolls a random number fom 1 to 100
 def roll_100(data):
-    n = random.randrange(0, 101)
+    n = random.randrange(1, 101)
     return(f'{str(data.author)[:-5]} hat eine {n} gewürfelt')
         
 def throw_coin():
